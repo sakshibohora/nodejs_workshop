8f53254d61f172fec1b93636968cfaf437089e7e
sakshibohora <46265763+sakshibohora@users.noreply.github.com>
2019-01-08 11:55:24 +0530

    Add files via upload

---
+++ http_uppercaserer.js
@@ -0,0 +1,11 @@
+var map=require('through2-map');
+var http=require('http');
+var server = http.createServer(function (req, res){
+	if(req.method=='POST'){
+	     res.pipe(map(function (chunk) {  
+	       return chunk.toString().toUpperCase()  
+	     })).pipe(res)
+	 }
+ });
+
+server.listen(process.argv[2]);
